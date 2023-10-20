3d21ddbffc8b63202c9d9f459260ca55720ed0f8
NazaGonella <80429205+NazaGonella@users.noreply.github.com>
2023-10-20 12:11:06 -0300

    Update SurfaceLevel0.java

--- TP3/SurfaceLevel0.java
+++ TP3/SurfaceLevel0.java
@@ -2,21 +2,22 @@ package nemo;
 
 public class SurfaceLevel0 extends Surface {
 
-	@Override
 	public Surface moveUp() {
 		return this;
 	}
 
-	@Override
 	public Surface moveDown() {
 		// TODO Auto-generated method stub
 		return new SurfaceLevel1();
 	}
 
-	@Override
 	public Surface releaseCapsule() {
 		// TODO Auto-generated method stub
 		return this;
+	}
+
+	public int getDepth() {
+		return 0;
 	}
 
 }
